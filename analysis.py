e6dcd604b6cbe7c028c2564c51edcc806305665e
snegi6 <negishivs@gmail.com>
2023-01-19 17:27:52 -0700

    python script

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# first some initial setup of libraries we will be using
+import pandas as pd # this is the main library used in python for manipulating data. 
+import json # this is a library to parse json (javascript object notation)
+from urllib.request import urlopen # library to read data from a URL
+import matplotlib.pyplot as plt # visualization library we will use that integrates with pandas
+
+
